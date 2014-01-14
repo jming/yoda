e28d4940c62053d4fa83a656f0a07e45c8752fe4
jming <jming@college.harvard.edu>
2014-01-14 15:29:22 -0500

    clean up js

--- mockup/script/app.js
+++ mockup/script/app.js
@@ -1,6 +1,10 @@
-// set up database
+
+/* DATABASE SET UP FUNCTIONS */
+
+// open database
 var db = openDatabase("Test", "1.0", "Test", 65535);
 
+// create concerns table
 db.transaction (function (transaction) {
 	var sql = "CREATE TABLE IF NOT EXISTS concerns "
 		+ " (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
@@ -22,6 +26,7 @@ db.transaction (function (transaction) {
 	);
 });
 
+// create doctors table
 db.transaction (function (transaction) {
 	var sql = "CREATE TABLE IF NOT EXISTS doctors "
 		+ " (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," 
@@ -37,7 +42,10 @@ db.transaction (function (transaction) {
 	);
 });
 
+// drop tables
 $(document).on('tap', '#clear-all-concerns', function () {
+
+	// drop concerns table
 	db.transaction (function (transaction) {
 		var sql = "DROP TABLE concerns";
 		transaction.executeSql(
@@ -51,6 +59,8 @@ $(document).on('tap', '#clear-all-concerns', function () {
 			}
 		);
 	});
+
+	// drop doctors table
 	db.transaction (function (transaction) {
 		var sql = "DROP TABLE doctors";
 		transaction.executeSql(
@@ -64,77 +74,27 @@ $(document).on('tap', '#clear-all-concerns', function () {
 			}
 		);
 	});
-	
 });
 
-// TODO: what should the default sort be??
+/* DISPLAY FUNCTIONS */
+
 var DEFAULT_SORT = "date";
+
 display_concerns(DEFAULT_SORT)
 
-// navigate to add concern page
-$(document).on('tap', '#add-concern-button', function() {
-	// delete the values in everything
-	$('#concernName').val('');
- 	$('#concern-notes').val('');
-	// change heading
-	$('#add-concerns-heading').text('New Concern');
-	// make everything not disabled
-	$('#concernName').prop('disabled', false).trigger('create');
-	$('#date-field').css('display', 'none');
-	$('#concern-notes').prop('disabled', false).trigger('create');
-	$('#add-concern-action').css('display', 'block');
-	$('#add-doctor-button').css('display','block');
-
-	$('#add-concern').on('pageshow', function() {
-		if ($('#concernName').val() == "") {
-			$('#urgency-choices input').checkboxradio('enable');
-			$('#urgency-choices input').prop('checked', false);
-			$('#urgency-choices input').checkboxradio('refresh');
-
-			display_assigned_choices();
-
-			$('#assigned-choices input').checkboxradio('enable');
-			$('#assigned-choices input').prop('checked', false);
-			$('#assigned-choices input').checkboxradio('refresh');
-		}
-	});
-});
-
-// add a doctor
-$(document).on('tap', '#add-doctor-action', function () {
-	console.log('derp');
-	db.transaction(function(transaction) {
-		var sql = 'INSERT INTO doctors (name, specialty) VALUES (?, ?)';
-		transaction.executeSql(
-			sql,
-			[$('#doctor-name').val(), $('#doctor-specialty').val()],
-			function(transaction, result) {
-				$i = result.insertId;
-				$('#doctor-name').val('');
-				$('#doctor-specialty').val('');
-				display_assigned_choices();
-				setTimeout(function() {
-					console.log('here', 'doctor-' + $i);
-					$('#doctor-' + $i).prop('checked', true).checkboxradio('refresh');
-				}, 100);
-			},
-			function(transaction, error) {
-				console.error(error);
-			}
-		);
-	})
-});
-
+// display possible doctors
 function display_assigned_choices() {
-	// console.log('displaying choices');
 	db.transaction(function (transaction) {
 		var sql = 'SELECT * FROM doctors';
 		transaction.executeSql(
 			sql,
 			undefined,
 			function(transaction, result) {
+				// check if there are doctors
 				if (result.rows.length) {
 					$('#no-doctor').css('display', 'none');
+
+					// append html for new doctors
 					var doctors_displayed = $('#assigned-choices-other input').length
 					for (var i=doctors_displayed; i<result.rows.length; i++) {
 						var row = result.rows.item(i);
@@ -153,29 +113,105 @@ function display_assigned_choices() {
 	});
 }
 
-// Add concern
+/* ADD FUNCTIONS */
+
+// navigate to *clean* add concern page
+$(document).on('tap', '#add-concern-button', function() {
+
+	// delete the values in everything
+	$('#concernName').val('');
+ 	$('#concern-notes').val('');
+
+	// change heading
+	$('#add-concerns-heading').text('New Concern');
+
+	// make everything not disabled
+	$('#concernName').prop('disabled', false).trigger('create');
+	$('#date-field').css('display', 'none');
+	$('#concern-notes').prop('disabled', false).trigger('create');
+
+	// display button
+	$('#add-concern-action').css('display', 'block');
+	$('#add-doctor-button').css('display','block');
+
+	// wait for pageshow
+	$('#add-concern').on('pageshow', function() {
+
+		// enable checkboxes if not on display-details version
+		if ($('#concernName').val() == "") {
+			$('#urgency-choices input').checkboxradio('enable');
+			$('#urgency-choices input').prop('checked', false);
+			$('#urgency-choices input').checkboxradio('refresh');
+
+			display_assigned_choices();
+
+			$('#assigned-choices input').checkboxradio('enable');
+			$('#assigned-choices input').prop('checked', false);
+			$('#assigned-choices input').checkboxradio('refresh');
+		}
+	});
+});
+
+// add a doctor
+$(document).on('tap', '#add-doctor-action', function () {
+
+	db.transaction(function(transaction) {
+		var sql = 'INSERT INTO doctors (name, specialty) VALUES (?, ?)';
+		transaction.executeSql(
+			sql,
+			[$('#doctor-name').val(), $('#doctor-specialty').val()],
+			function(transaction, result) {
+
+				// clear value
+				$i = result.insertId;
+				$('#doctor-name').val('');
+				$('#doctor-specialty').val('');
+
+				// display choices with new doctor
+				display_assigned_choices();
+
+				// automatically check doctor off
+				setTimeout(function() {
+					$('#doctor-' + $i).prop('checked', true).checkboxradio('refresh');
+				}, 100);
+			},
+
+			function(transaction, error) {
+				console.error(error);
+			}
+		);
+	})
+});
+
+// Add concern action 
 $(document).on("tap", "#add-concern-action", function() {
+
+	// get values from form
 	var $concern = $("#concernName").val();
 	var $i = 0;
 	var urgency = $('#urgency-choices input:checked').attr('id');
 	var assigned_checked = $('#assigned-choices input:checked');
+
+	// check if all required fields are filled in
 	if (($concern != "") && (urgency != null) && assigned_checked.length > 0) {
 		
+		// create list of "assgined-to"
 		var assigned_to = assigned_checked.attr('id');
 		for (var n = 1; n < assigned_checked.length; n++) {
 			assigned_to += ',' + $(assigned_checked[n]).attr('id');
 		}
-		console.log(assigned_to);
+
 		db.transaction(function (transaction) {
 			var sql = "INSERT INTO concerns (name, date, assigned, urgency, notes, ordering) VALUES (?, ?, ?, ?, ?, ?)";
 			transaction.executeSql(
 				sql, 
 				[$concern, new Date(), assigned_to, urgency, $('#concern-notes').val(), 0], 
 				function(transaction, result) {
-					console.log([$concern, new Date(), assigned_to, urgency, 0]);
+
 					$("#no-concern").css("display","none");
 					$i = result.insertId;
 
+					// update ordering
 					db.transaction(function (transaction) {
 						var sql = "UPDATE concerns SET ordering=" + $("#concern-list").children().size() + " WHERE id=" + $i;
 						transaction.executeSql(
@@ -187,18 +223,22 @@ $(document).on("tap", "#add-concern-action", function() {
 							}
 						);
 					});
-					// TODO: clear all inputs
 
+					// clear inputs
 					for (var i=0; i < assigned_checked.length; i++) {
 						assigned_checked[i].click();
 					}
 					$('#urgency-choices #High').prop('checked', true);
-
-					display_concerns(DEFAULT_SORT);
-					$.mobile.changePage("#concern-list-view");		
 					$("#concernName").val("");
 					$('#concern-notes').val('');
+
+					// display default
+					display_concerns(DEFAULT_SORT);
+
+					// change page
+					$.mobile.changePage("#concern-list-view");		
 				},
+
 				function (transaction, error) {
 					console.error(error);
 				}
@@ -209,10 +249,15 @@ $(document).on("tap", "#add-concern-action", function() {
 	}
 });
 
+/* REVIEW CONCERNS */
 
-// Remove concern
+// Remove concern (delete)
 $(document).on("tap", "#concern-list.patient-view li a.close", function() {
+	
+	// get id
 	$i = $(this).parent().attr("id");
+
+	// change ordering to -1 in database (basically removes but just hides)
 	db.transaction(function (transaction) {
 		var sql = "UPDATE concerns SET ordering=-1 WHERE id=" + $i;
 		transaction.executeSql(
@@ -224,44 +269,47 @@ $(document).on("tap", "#concern-list.patient-view li a.close", function() {
 			}
 		);
 	});
+
+	// removal animation
 	$(this).parent().slideUp('normal', function(){
 			$(this).remove();
 		}
 	);
-	 	
-	return false;
 });
 
-// Disable concern during visit
+// Mark concern as reviewed during visit
 $(document).on("tap", "#concern-list.doctor-view li a.close", function() {
+	
+	// make the UI animation disabled
 	$(this).parent().addClass('ui-disabled');
-	// TODO: change in database
+	
+	// mark concern in database as reviewed with review date
 	var liId = $(this).parent().attr('id');
-	console.log('id', $(this).parent().attr('id'));
 	db.transaction(function(transaction) {
 		var sql = "UPDATE concerns SET checked='" + new Date() + "' WHERE id='" + liId + "'";
-		console.log('concern-list change sql', sql);
 		transaction.executeSql(
 			sql,
 			undefined,
-			function(transaction, result) {
-				console.log(result);
-			},
+			function(transaction, result) {},
 			function(transaction, err) {
 				console.error(err);
 			}
 		);
 	});
-	return false;
 });
 
 // sort order of concerns
 $(document).bind('pageinit', function() {
 
+	// make it "sortable"
     $( "#concern-list" ).sortable();
     $( "#concern-list" ).disableSelection();
     $( "#concern-list" ).bind( "sortstop", function(event, ui) {
+    	
+    	// make each sortable
     	var $concernLi = $("#concern-list li");
+		
+    	// update ordering
 		db.transaction(function (transaction) {
 			for (var i = 0; i < $concernLi.length; i++) {
 				var elementId = $concernLi[i].id;
@@ -271,7 +319,6 @@ $(document).bind('pageinit', function() {
 					sql,
 					undefined,
 					function (transaction, result) {
-						console.log(result);
 					},
 					function (transaction, error) {
 						console.error(error);
@@ -279,6 +326,8 @@ $(document).bind('pageinit', function() {
 				);
 			}
 		});
+		
+		// refresh views
 		$('ul').listview('refresh');
     	$('#concern-list').listview('refresh');
     });
@@ -289,47 +338,61 @@ $(function () {
 	$('#concern-list-sort').change(function() {
 
 		var selected = $('#concern-list-sort option:selected').val();
+		
+		// change display
 		display_concerns(selected);
 
-		return false
 	});
-})
+});
 
 // set details for given concern!
 function set_details(id) {
 
 	// change heading
 	$('#add-concerns-heading').text('Concern Details');
+
 	// make everything disabled
 	$('#concernName').prop('disabled', true).trigger('create');
 	$('#date-field').css('display','inline');
 	$('#concern-notes').prop('disabled', true).trigger('create');
+
 	// show the doctors
 	display_assigned_choices();
+
 	// hide the button
 	$('#add-concern-action').css('display', 'none');
 	$('#add-doctor-button').css('display','none');
+
+	// get values for given concern
 	db.transaction(function (transaction) {
 		var sql = "SELECT * FROM concerns WHERE id=" + id;
 		transaction.executeSql(
 			sql,
 			undefined,
 			function (transaction, result) {
-				// console.log('inside set details')
+
 				var item = result.rows.item(0);
-				// console.log(item.assigned, item.urgency)
+
+				// set name
 				$("#concernName")[0].value = item.name;
+
+				// set date
 				$('#detail-date')[0].value = item.date;
+
+				// set assigned to
 				var assigned_to = item.assigned.split(',');
-				console.log(assigned_to);
 				for (var n = 0; n < assigned_to.length; n++) {
 					$('#assigned-choices #' + assigned_to[n]).prop('checked', true);
 				}
 				$('#assigned-choices input').checkboxradio('refresh');
 				$('#assigned-choices input').checkboxradio('disable');
+
+				// set urgency
 			    $('#urgency-choices ' + '#' + item.urgency).prop('checked', true);
 			    $('#urgency-choices input').checkboxradio('refresh');
 			    $('#urgency-choices input').checkboxradio('disable');
+
+			    // set notes
 			    $('#concern-notes')[0].value = item.notes;
 			},
 			function (transaction, error) {
@@ -339,33 +402,40 @@ function set_details(id) {
 	})
 }
 
+// display concerns
 function display_concerns_filtered(sorted_by, filtered_by) {
 
 	// fix the selector
 	$("#concern-list-sort").val(sorted_by).attr('selected', true).siblings('option').removeAttr('selected');
 	$('#doctor-select').selectmenu('refresh', true);
-
-	console.log('displaying concerns sorted by ' + sorted_by + ' filtered by ' + filtered_by);
 	
 	db.transaction( function(transaction) {
+
 		var sql = "SELECT * FROM concerns WHERE checked IS NULL"
+
+		// add filtered by params
 		if (filtered_by != null) {
 			sql += " AND assigned LIKE '%doctor-" + filtered_by + "%'";
 		}
+		
+		// add sorted by params
 		if (sorted_by == 'date') {
 			sql += " ORDER BY " + sorted_by + ' DESC';
 		} else {
 			sql += " ORDER BY " + sorted_by;
 		}
-		console.log('display_concerns_filtered sql ' + sql);
+
 		transaction.executeSql(
 			sql, 
 			undefined, 
 			function (transaction, result) {
-				console.log(result.rows);
 				if (result.rows.length) {
+
+					// remove previous elements
 					$("#concern-list").empty();
 					$("#no-concern").css("display","none");
+
+					// add each list item
 					for (var i = 0; i < result.rows.length; i++) {
 						var row = result.rows.item(i);
 						if (row.ordering != -1) {
@@ -391,18 +461,14 @@ function display_concerns_filtered(sorted_by, filtered_by) {
 	});
 }
 
+// basic display without filter
 function display_concerns(sorted_by) {
 	display_concerns_filtered(sorted_by, null);
 }
 
+// select which doctor visiting
 $(document).on('tap', '#visit-begin-button', function() {
-	console.log('visit-begin-button tapped');
-	// $('#visit-prepare-doctors').popup();
-	// setTimeout(function() {
-		
-	// }, 0);
 
-	//fill visit-doctor-select
 	db.transaction(function (transaction) {
 		var sql = 'SELECT * FROM doctors';
 		transaction.executeSql(
@@ -410,6 +476,8 @@ $(document).on('tap', '#visit-begin-button', function() {
 			undefined,
 			function(transaction, result) {
 				if (result.rows.length) {
+
+					// display possible doctors
 					$('#no-doctors-select').css('display', 'none');
 					$('#visit-doctor-form').css('display', 'block');
 					for (var i=0; i<result.rows.length; i++) {
@@ -427,26 +495,31 @@ $(document).on('tap', '#visit-begin-button', function() {
 	});
 })
 
+// after selecting doctor, begin visit
 $(document).on('tap', '#visit-doctor-selected', function() {
 
+	// change display of information
 	$('#visit-information').css('display', 'block');
 	$('#visit-doctor-information').empty();
 	var doctorId = $('#visit-doctor-select option:selected').val();
-	console.log(doctorId);
+
+	// get doctor information
 	db.transaction(function(transaction) {
 		var sql = 'SELECT * FROM doctors WHERE id=' + doctorId;
 		transaction.executeSql(
 			sql,
 			undefined,
 			function(transaction, result) {
+
+				// append doctor information
 				var doctorInfo = result.rows.item(0);
-				console.log(doctorInfo);
 				$('#visit-doctor-information').append(
 					"<h2>" + doctorInfo.name + "</h2>"
 					+ "<p>Specialty: " + doctorInfo.specialty
 					+ "</p><p>Visit date: " + new Date() + "</p>"
-					// TODO: change formatting of date
 				);
+
+				// display filtered concerns
 				display_concerns_filtered(DEFAULT_SORT, doctorId);
 			},
 			function(transaction, error) {
@@ -455,6 +528,7 @@ $(document).on('tap', '#visit-doctor-selected', function() {
 		);
 	});
 
+	// hide and display other elements of the page
 	$('#concern-list-header').text('Visit')
 	$('#add-concern-button').css('display', 'none');
 	$('#clear-all-concerns').css('display', 'none');
@@ -466,10 +540,12 @@ $(document).on('tap', '#visit-doctor-selected', function() {
 	$('#back-concern-view').css('display', 'block');
 });
 
+// go back to concern view
 $(document).on('tap', '#back-concern-view', function() {
 	location.reload();
 });
 
+// complete visit
 $(document).on('tap', '#visit-complete-button', function() {
 	location.reload();
 });
